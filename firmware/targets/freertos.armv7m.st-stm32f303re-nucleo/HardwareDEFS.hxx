915a06830fa999f8882959c4c423f6a172505bce
Robert Heller <heller@deepsoft.com>
2023-04-12 16:36:21 -0400

    Add firmware support for additional outputs and start on Crossing Logic.

---
+++ firmware/targets/freertos.armv7m.st-stm32f303re-nucleo/HardwareDEFS.hxx
@@ -0,0 +1,58 @@
+// -!- c++ -!- //////////////////////////////////////////////////////////////
+//
+//  System        : 
+//  Module        : 
+//  Object Name   : $RCSfile$
+//  Revision      : $Revision$
+//  Date          : $Date$
+//  Author        : $Author$
+//  Created By    : Robert Heller
+//  Created       : Wed Apr 12 11:38:19 2023
+//  Last Modified : <230412.1143>
+//
+//  Description	
+//
+//  Notes
+//
+//  History
+//	
+/////////////////////////////////////////////////////////////////////////////
+/// @copyright
+///    Copyright (C) 2023  Robert Heller D/B/A Deepwoods Software
+///			51 Locke Hill Road
+///			Wendell, MA 01379-9728
+///
+///    This program is free software; you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation; either version 2 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program; if not, write to the Free Software
+///    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/// @file HardwareDEFS.hxx
+/// @author Robert Heller
+/// @date Wed Apr 12 11:38:19 2023
+/// 
+///
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef __HARDWAREDEFS_HXX
+#define __HARDWAREDEFS_HXX
+
+
+#define RIR4ADDRESS 0x30
+//#define RIR4ADDRESS2 0x31
+
+#define NUM_SERVOS 4
+#define NUM_SIGNALS 4
+#define NUM_STALL 2
+#define NUM_SSR 2
+
+#endif // __HARDWAREDEFS_HXX
+
